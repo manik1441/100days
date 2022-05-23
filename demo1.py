2645ec66f1fb812674fcf1de14b7581456f42786
manik1441 <33223667+manik1441@users.noreply.github.com>
2022-05-23 17:57:07 +0530

    New files
    New files

---
+++ demo1.py
@@ -0,0 +1,18 @@
+
+def test_m1():
+    assert 1 == 1
+
+
+def test_m2():
+    assert 1 == 1
+
+def test_m3():
+    assert 1 == 1
+
+
+def test_m4():
+    assert 1 == 1
+
+
+def test_m5():
+    assert 1 == 2
---
+++ test.py
@@ -0,0 +1,7 @@
+import time
+import pyautogui
+
+while True:
+    time.sleep(2)
+    pyautogui.moveTo(100, 100)
+    pyautogui.moveTo(100, 150)
