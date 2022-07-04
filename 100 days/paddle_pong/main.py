507e95d1c229caedca8fb469389d85a5448ed2b1
manik <manik1441@gmail.com>
2022-07-05 00:29:37 +0530

    paddle and road cross game

--- 100 days/paddle_pong/main.py
+++ 100 days/paddle_pong/main.py
@@ -1,4 +1,8 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
+from ball import Ball
+from scoreboard import  Scoreboard
+import time
 
 screen = Screen()
 screen.bgcolor('black')
@@ -6,28 +10,41 @@ screen.setup(width=800, height=600)
 screen.title("Pong")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape('square')
-paddle.color('white')
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
-
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+
+    # when ball hits the wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # when ball hits the right paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    #when ball miss the r_paddle
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    #when ball miss the r_paddle
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+
 
 screen.exitonclick()
--- 100 days/snake/scoreboard.py
+++ 100 days/snake/scoreboard.py
@@ -9,6 +9,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open('data.txt') as f:
+            self.highscore = int(f.read())
         self.color('white')
         self.penup()
         self.goto(0, 270)
@@ -16,14 +18,21 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGN, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGN, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGN, font=FONT)
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGN, font=FONT)
 
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open('data.txt', 'w') as w:
+                w.write(f"{self.highscore}")
+        self.score = 0
+        self.update_scoreboard()
